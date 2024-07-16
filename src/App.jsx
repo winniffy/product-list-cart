e790ecda1207dbc20e872d944eee9f924214c6c6
winniffy <winniffy@gmail.com>
2024-07-16 16:48:49 +0100

    reset css and add css variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import './variables.css'
 import Products from './data.json'
-// import pic from './assets/images/image-creme-brulee-desktop.jpg'
 
 function App() {
 
   return (
     <>
-        <h1>Vite + React</h1>
+        <h1>Desserts</h1>
         {
           Products && Products.map((product => {
             return (
